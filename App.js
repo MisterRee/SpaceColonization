5851f7711675b681bb4910886b7e130455ad5c3b
Danny Lee <DannyLeeNewMedia@gmail.com>
2017-12-04 12:11:10 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 const express = require( 'express' );
 const App = express();
 
-const server = require( 'http' ).createServer( App );
-
 App.use( express.static( __dirname + '/dist' ) );
 
 App.get( '/', function( req, res ){
   res.sendFile( __dirname + '/dist/index.html' );
 });
 
-server.listen( 3000 );
+App.set( 'port', process.env.PORT || process.env.NODE_PORT || 3000 );
 
+App.listen( App.get( 'port' ) );
 // This was used solely for local development purposes to avoid CORS errors
