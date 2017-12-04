c84a55d4bea309ed4a715752951cd3b804f0b15d
Danny Lee <dxl1720@rit.edu>
2017-12-04 11:18:14 -0500

    Routing fixes

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-const express = require( 'express' );
-const App = express();
 
-const server = require( 'http' ).createServer( App );
+  const express = require( 'express' );
+  const http  = require( 'http' );
+  const fs = require( 'fs' );
+
+  const App = express();
 
 App.use( express.static( __dirname + '/dist' ) );
 
@@ -9,6 +11,7 @@ App.get( '/', function( req, res ){
   res.sendFile( __dirname + '/dist/index.html' );
 });
 
-server.listen( 3000 );
+App.set( 'port', process.env.PORT || process.env.NODE_PORT || 3000 );
 
+App.listen( App.get( 'port' ) );
 // This was used solely for local development purposes to avoid CORS errors
